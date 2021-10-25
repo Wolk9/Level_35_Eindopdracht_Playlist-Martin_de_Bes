b0316bb6e7ba9c2a558579a5bd5af744c8f547f4
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-10-25 14:23:49 +0200

    fix compoeEnnhancers

--- src/state/store.js
+++ src/state/store.js
@@ -1,7 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers/index";
 import thunk from "redux-thunk";
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+const composeEnhancer = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export const store = createStore(reducers, {}, composeEnhancer);
 
 //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
